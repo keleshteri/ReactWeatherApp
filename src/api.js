3cd325d714a6f838aa25832166640946b986897a
Mohammad Mehdi Shaban keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-07-17 00:36:03 +0800

    added api endpoint & update search

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e8456a201amsh56f5ecedc805478p186e82jsnfe0a96dce491',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
