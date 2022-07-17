acac501d11c167fcf42711aa39009ef808d81471
Mohammad Mehdi Shaban keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-07-17 15:48:38 +0800

    update current-weather

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="container">
     <Search onSearchChange={handleOnSearchChange}/>
-    <CurrentWeather />
+    {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
