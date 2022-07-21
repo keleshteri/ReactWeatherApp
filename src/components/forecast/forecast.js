6e97933a992d53b5587e168aed078d704d752496
Mohammad Mehdi Shaban keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-07-21 10:15:27 +0800

    added forecast

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,15 @@
+import { Accordion } from "react-accessible-accordion";
+
+const Forecast = ({data}) => {
+  return(
+  <>
+    <label className="title">Daily</label>
+    <Accordion allowZeroExpanded>
+
+    </Accordion>
+  </>
+  );
+   
+};
+
+export default Forecast;
